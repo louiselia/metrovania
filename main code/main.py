1ab44de3562cf22591b5e323ae17529e7989a13f
luis <kalulu@gmx.at>
2024-02-19 19:23:22 +0100

    main loop

--- main code/main.py
+++ main code/main.py
@@ -1,1 +1,22 @@
-import pygame
+import pygame, sys
+
+
+pygame.init()
+
+
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption('t-shirts')
+clock = pygame.time.Clock()
+
+while True:
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    pygame.display.update()
+    clock.tick(60)
+
