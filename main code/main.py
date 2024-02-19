7799b63dcf9dd02b736fd2f34562be9195c69863
luis <kalulu@gmx.at>
2024-02-19 19:45:30 +0100

    d un a screen moving

--- main code/main.py
+++ main code/main.py
@@ -7,6 +7,7 @@ pygame.init()
 screen = pygame.display.set_mode((2000,1200))
 pygame.display.set_caption('t-shirts')
 clock = pygame.time.Clock()
+bg_y = 0
 
 while True:
 
@@ -15,6 +16,16 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                bg_y += 1
+                print('pressed')
+
+            if event.key == pygame.K_a:
+                bg_y -= 1
+                print('pressed')
+
+
 
 
     pygame.display.update()
