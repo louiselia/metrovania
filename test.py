235ad02fd013738d8c1a2f551eba17088543eb2a
louiselia <lianatter2007@gmail.com>
2024-02-23 09:56:32 +0100

    working on the player animation

--- test.py
+++ test.py
@@ -1,3 +1,27 @@
-import pytmx
+import pygame
+import sys
 
-map = pytmx.load_pygame('images/map/starting are/tiled/starting_area.tmx')
+class Player(pygame.sprite.sprite):
+
+
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption("Test Animation")
+
+moving_sprites = pygame.sprite.Group()
+player = Player(100,100)
+moving_sprites = add(player)
+
+loop = True
+while loop:
+ for event in pygame.event.get():
+  if event.type == pygame.QUIT:
+   pygame.quit()
+   sys.exit()
+ screen.fill((255,255,255))
+ moving_sprites.draw(screen)
+ pygame.display.flip()
+ clock.tick(60)
+
