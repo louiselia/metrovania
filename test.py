dfddb61e5f83d86bb17532b9887da4f481002023
louiselia <lianatter2007@gmail.com>
2024-02-25 01:57:45 +0100

    progress with animation, one or two classes still missing

--- test.py
+++ test.py
@@ -1,13 +1,42 @@
 import pygame
 import sys
 from pygame.locals import *
-'''
+
 pygame.init()
 
 WINDOW_SIZE = (800, 600)
 screen = pygame.display.set_mode(WINDOW_SIZE)
 
 pygame.display.set_caption("Character Example")
+
+sprite_sheet = pygame.image.load("images/sprites/character/Characters/Knight_anin.png")
+
+class spritesheet:
+    def __int__(self,filename, cols, rows):
+        self.sheet = pygame.image.load(filename)
+        self.cols = cols
+        self.rows = rows
+
+        self.rect = self.sheet.get_rect()
+        w = self.cellWidth = self.rect.width / cols
+        h = self.cellHeigt = self.rect.height / rows
+        halfw = self.cellCenter = (w/2)
+        halfh = self.cellCenter = (h/2)
+
+        # self.cells = list([(index % cols * w, index / cols * h,w,h) for index in range(self.totalCellCount)]) wtf??????
+        self.cells = []
+        for index in range(self.totalCellCount):
+            cell_x = index % cols * w
+            cell_y = index // cols * h
+            cell_width = w
+            cell_height = h
+            self.cells.append((cell_x, cell_y, cell_width, cell_height))
+
+        self.handle = [(0, 0), (-halfw, 0), (-w, 0), (0, -halfh), (-halfw, -halfw), (-w, -halfh), (0, -h), (-halfw, -h), (-w, -h)]
+
+    def draw(self,surface, cellindex, x, y, handle = 0):
+        surface.blit(self.sheet,(x + self.handle[handle][0], y + self.handle[handle][1]),self.cells[cellindex])
+
 
 class Character:
     def __init__(self, image, x, y):
@@ -17,8 +46,7 @@ class Character:
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
-
-player = Character("images/sprites/character/Characters/king.png", 100, 100)
+player = Character("images/sprites/character/Characters/Knight_anin.png", 100, 100)
 
 while True:
 
@@ -27,19 +55,8 @@ while True:
             pygame.quit()
             sys.exit()
 
-        elif event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                player.x -= 10
-            elif event.key == K_RIGHT:
-                player.x += 10
-            elif event.key == K_UP:
-                player.y -= 10
-            elif event.key == K_DOWN:
-                player.y += 10
-
     screen.fill("black")
     player.draw(screen)
     pygame.display.update()
-'''
 
 
