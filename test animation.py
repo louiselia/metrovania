76ca3301e594515d73aa8992976179f9e8b38dc1
louiselia <lianatter2007@gmail.com>
2024-02-25 21:56:13 +0100

    help

--- test animation.py
+++ test animation.py
@@ -2,17 +2,24 @@ import pygame
 import sys
 
 
-def frame(fnum, f_w, f_h):
+def frame(fnum, f_w, f_h, f_y):
 
     # this determines how big the singular animation parts will be
     # and then puts the places of each tile inside the sprite sheet provided in a list that is than returned
 
     returner = []   # returner creates a list containing every frame in the current row
-    for i in range(fnum):   # goes through the number of images
-        x = pygame.Rect(f_w * i, 0, f_w, f_h)   # creates a rect, with x = width times pos, y = 0 and the width and height of the rects
-        returner.append(x)
+    for j in range(7):
+        liste = []
+        for i in range(fnum):   # goes through the number of images
+
+            x = pygame.Rect(f_w * i, f_y * j, f_w, f_h)   # creates a rect, with x = width times pos, y = 0 and the width and height of the rects
+            liste.append(x)
+
+        returner.append(liste)
 
     return returner
+
+
 
 def ftimer(time, fnow, fnum, lasttime):
 # this looks if the time ist < 100 this determins the time every part of an animation uses
@@ -33,9 +40,12 @@ def main():
 
     fnow = 0
     fnum = 7
+    lasttime = 0
+
     f_w = 48
     f_h = 32
-    lasttime = 0
+    f_y = 0
+
 
     WIDTH = 300
     HEIGHT = 300
@@ -43,7 +53,7 @@ def main():
     pygame.display.set_caption("Animation Test")
     sprite_sheet = pygame.image.load("images/sprites/character/Characters/Knight_anin.png")
 
-    frame_list = frame(fnum, f_w, f_h)
+    frame_list = frame(fnum, f_w, f_h, f_y)
 
     while True:
         time = pygame.time.get_ticks()
@@ -56,7 +66,7 @@ def main():
 
         screen.fill("black")
 
-        screen.blit(sprite_sheet,(150, 150), frame_list[fnow])
+        screen.blit(sprite_sheet,(150, 150), frame_list[fnow[fnow]])
 
         pygame.display.update()
 
