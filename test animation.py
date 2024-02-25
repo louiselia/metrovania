e34ae6d49dd3d352ffacb297ac00a3d2c5ca5913
louiselia <lianatter2007@gmail.com>
2024-02-25 20:38:16 +0100

    progress in test

--- test animation.py
+++ test animation.py
@@ -7,16 +7,24 @@ def frame(fnum, f_w, f_h):
     # this determines how big the singular animation parts will be
     # and then puts the places of each tile inside the sprite sheet provided in a list that is than returned
 
-    returner = []
-    for i in range(fnum):
-        x = pygame.Rect(f_w * i, 0, f_w, f_h)
+    returner = []   # returner creates a list containing every frame in the current row
+    for i in range(fnum):   # goes through the number of images
+        x = pygame.Rect(f_w * i, 0, f_w, f_h)   # creates a rect, with x = width times pos, y = 0 and the width and height of the rects
         returner.append(x)
 
     return returner
 
-WIDTH = 64
-HEIGHT = 64
+fnum = 7
+f_w = 48
+f_h = 32
+
+WIDTH = 300
+HEIGHT = 300
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Animation Test")
+sprite_sheet = pygame.image.load("images/sprites/character/Characters/Knight_anin.png")
+
+frame_list = frame(fnum, f_w, f_h)
 
 while True:
 
@@ -25,5 +33,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    knight = sprite_sheet.get_rect(frame_list[0])
+    pygame.draw(knight)
     screen.fill("black")
     pygame.display.update()
