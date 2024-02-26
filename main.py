40b0694b70f5aed10cac08faf94b2ca6cbeb54d0
louiselia <lianatter2007@gmail.com>
2024-02-26 09:34:57 +0100

    animation not working yet

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 import pygame, sys, pytmx
 
-def frame(fnum, f_w, f_h):
+def frame(fnum, f_w, f_h, f_y):
 
     # this determines how big the singular animation parts will be
     # and then puts the places of each tile inside the sprite sheet provided in a list that is than returned
 
-    returner = []
-    for i in range(fnum):
-        x = pygame.Rect(f_w * i, 0, f_w, f_h)
-        returner.append(x)
+    returner = []   # returner creates a list containing every frame in the current row
+    for j in range(7):
+        liste = []
+
+        if j == 2 or j == 3:
+            fnum = 1
+
+        elif j == 4:
+            fnum = 14
+
+        elif j == 5:
+            fnum = 2
+
+        elif j == 6:
+            fnum = 6
+
+        for i in range(fnum):   # goes through the number of images
+
+            x = pygame.Rect(f_w * i, f_y * j, f_w, f_h)   # creates a rect, with x = width times pos, y = 0 and the width and height of the rects
+            liste.append(x)
+
+        returner.append(liste)
 
     return returner
 
-def ftimer(time, fnow, fnum):
-#   this looks if the time ist < 100 this determins the time every part of an animation uses
 
+
+
+def ftimer(time, fnow, fnum, lasttime):
+# this looks if the time ist < 100 this determins the time every part of an animation uses
     fdelay = 100
-    lasttime = pygame.time.get_ticks()
+    if time - lasttime > fdelay:
+        fnow = (fnow + 1) % fnum
+        lasttime = time
+    return fnow, lasttime
 
-    if time - lasttime < fdelay:
-        fnow = (fnow +1) % fnum
-        return time, fnow
 
 def map_lister(tmxfile):
 #   this takes every tile that is a instance(that means it will have to be drawn) and puts it in a list
@@ -57,9 +77,17 @@ def main():
     clock = pygame.time.Clock()
     pygame.key.set_repeat(1)
 
+    fnow = 0
+    fnum = 7
+    lasttime = 0
+
+    f_w = 48
+    f_h = 32
+    f_y = 32
+
+    a_num = 0
     #frame zeugs
-    f_h = 16
-    f_w = 16
+
     fnum = 3 # frame count
     fnow = 0 # frame the programm is currently on
 
@@ -70,6 +98,10 @@ def main():
     #map zeugs
     map_list, map_data = map_lister('images/map/starting are/tiled/tsx/starting area.tmx') # gets two lists one withe the tiles in the rows and one with all the info from the given tmx file
 
+    frame_list = frame(fnum, f_w, f_h, f_y)
+
+    sprite_sheet = pygame.image.load("images/sprites/character/Characters/Knight_anin.png")
+
 #   player zeugs
 
 #    player = Character("images/sprites/character/Characters/king.png", screen.get_width() / 4, screen.get_height() / 4 * 3)
@@ -77,6 +109,7 @@ def main():
 
     while True:
 
+        pressed_keys = pygame.key.get_pressed()
         time = pygame.time.get_ticks()
 
         for event in pygame.event.get():
@@ -84,33 +117,35 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+            if pressed_keys[pygame.K_d] == True:
+                fnum = 6
+                a_num = 1
 
-            if event.type == pygame.KEYDOWN:
+            if pressed_keys[pygame.K_a] == True:
+                fnum = 6
+                a_num = 1
 
-                # left right input
-                if event.key == pygame.K_d:
-                    bg_x -= move_speed
-                    print('pressed')
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    fnum = 13
+                    a_num = 4
+                    fnow = 0
 
-                if event.key == pygame.K_a:
-                    bg_x += move_speed
-                    print('pressed')
+            if event.type == pygame.KEYUP or event.type == pygame.MOUSEBUTTONUP:
+                a_num = 0
+                fnum = 7
+                fnow = 0
+
+            #fnow, lasttime = ftimer(time, fnow, fnum, lasttime)
+
+            map_drawer(screen, map_list, map_data, bg_x, bg_y)
+            screen.blit(sprite_sheet, (150, 150), frame_list[a_num][fnow])
 
 
-                # up down inputs
-                if event.key == pygame.K_w:
-                    bg_y += move_speed
-                    print('pressed')
+        fnow, lasttime = ftimer(time, fnow, fnum, lasttime)
 
-                if event.key == pygame.K_s:
-                    bg_y -= move_speed
-                    print('pressed')
 
-        time, fnow = ftimer(time, fnow, fnum)
 
-        screen.fill((0, 27, 35))
-
-        map_drawer(screen, map_list, map_data, bg_x, bg_y)
 
         pygame.display.update()
         clock.tick(60)
