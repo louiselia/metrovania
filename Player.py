8fd713dd26fe3f2c3113565e48dee78b42282949
Luis <kalulu@gmx.at>
2024-02-26 09:46:29 +0100

    furthermore classed player

--- Player.py
+++ Player.py
@@ -5,3 +5,15 @@ class Player:
 		self.friction = -2
 		self.accseleration = .5
 		self.velocity = 0
+
+	def horizontalmove(self):
+		pass
+
+
+	def verticalmove(self):
+		pass
+
+
+	def update(self):
+		self.horizontalmove()
+		self.verticalmove()
