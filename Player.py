c0eade8feec2c6581265fc560e57e09b5c682a6b
Luis <kalulu@gmx.at>
2024-02-27 08:21:10 +0100

    furthermore classed player

--- Player.py
+++ Player.py
@@ -1,8 +1,18 @@
+import pygame.math
+
+
 class Player:
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
+		self.
+		self.
+		self.
+		self.isjumping = False
+		self.onground = False
+		self.gravity = 2.
 		self.friction = -2
+		self.position = pygame.math.Vector2(0,0)
 		self.accseleration = .5
 		self.velocity = 0
 
